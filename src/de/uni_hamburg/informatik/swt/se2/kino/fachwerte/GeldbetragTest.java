1addaea4752f233a3c02fb499c877b4304c3d860
Jan <jlbichel@gmail.com>
2021-06-30 09:49:12 +0200

    added testcases

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -1,11 +1,14 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
+import org.junit.Test;
+
 public class GeldbetragTest {
 
 	Geldbetrag _geldBetragEins;
 	Geldbetrag _geldBetragZwei;
 	
 	public GeldbetragTest() {
+<<<<<<< Updated upstream
 		_geldBetragEins = new Geldbetrag();
 		_geldBetragZwei = new Geldbetrag();
 	}
@@ -13,6 +16,23 @@ public class GeldbetragTest {
 	void testeAddiere()
 	{
 	
+=======
+
 	}
 
+	@Test
+	public void testeKonvertiereString() {
+
+>>>>>>> Stashed changes
+	}
+
+	@Test
+	public void testeHashCode() {
+
+	}
+
+	@Test
+	public void testeEquals() {
+
+	}
 }
