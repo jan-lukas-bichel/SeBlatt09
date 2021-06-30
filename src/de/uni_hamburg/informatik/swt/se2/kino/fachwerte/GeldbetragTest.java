8226241d7b58f6ef05e097077cfbf99b91e890da
Jan <jlbichel@gmail.com>
2021-06-30 10:26:56 +0200

    small fixes in teste multiplizieren

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -1,52 +1,43 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class GeldbetragTest
-{
-
-    Geldbetrag _geldBetragEins;
-    Geldbetrag _geldBetragZwei;
-    Geldbetrag _geldBetragDrei;
-
-    public void testeAddiere()
-    {
-
-    }
-
-    public void testeSubtrahieren()
-    {
-
-    }
-
-    public void testeIstSubtrahierenMoeglich()
-    {
-
-    }
+public class GeldbetragTest {
 
 	Geldbetrag _geldBetragEins;
 	Geldbetrag _geldBetragZwei;
 	Geldbetrag _geldBetragDrei;
-    
-    @Test
-    public void testeMultipliziere()
-    {
-        _geldBetragEins = Geldbetrag.get(20, 25);
-        _geldBetragZwei = Geldbetrag.get(20, 00);
 
-        assertTrue(Geldbetrag.get(40, 00), _geldBetragZwei.multipliziere(2));
-        assertFalse(Geldbetrag.get(81), _geldBetragEins.multipliziere(4));
+	public void testeAddiere() {
 
-    }
+	}
 
-    @Test
-    public void testeIstMultiplizierenMeoglich()
-    {
+	public void testeSubtrahieren() {
 
-    }
+	}
+
+	public void testeIstSubtrahierenMoeglich() {
+
+	}
+
+	@Test
+	public void testeMultipliziere() {
+		_geldBetragEins = Geldbetrag.select(20, 25);
+		_geldBetragZwei = Geldbetrag.select(20, 00);
+
+		assertTrue(Geldbetrag.select(40, 00), _geldBetragZwei.multipliziere(2));
+		assertFalse(Geldbetrag.select(81, 00), _geldBetragEins.multipliziere(4));
+
+	}
+
+	@Test
+	public void testeIstMultiplizierenMeoglich() {
+
+	}
 
 	@Test
 	public void testeKonvertiereString() {
