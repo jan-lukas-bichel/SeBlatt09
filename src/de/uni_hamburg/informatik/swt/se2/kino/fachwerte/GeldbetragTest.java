597f875e658ca1da332a48a1fa82a877ccb0548f
Bungi77 <67256811+Bungi77@users.noreply.github.com>
2021-06-30 09:46:50 +0200

    Update GeldbetragTest.java

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -2,8 +2,15 @@ package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
 public class GeldbetragTest {
 
+	Geldbetrag _geldbetrag;
+	
 	public GeldbetragTest() {
-		
+		_geldbetrag = new Geldbetrag();
+	}
+	
+	void testeAddiere()
+	{
+	
 	}
 
 }
