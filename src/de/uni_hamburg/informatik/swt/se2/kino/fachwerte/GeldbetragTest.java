ecb2c638f73db87b841bd7ea64394612e060eb8d
Chiara897 <chiarasteenbuck@gmx.de>
2021-06-30 10:17:37 +0200

    Update GeldbetragTest.java

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -1,34 +1,64 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-public class GeldbetragTest {
+public class GeldbetragTest
+{
 
-	Geldbetrag _geldBetragEins;
-	Geldbetrag _geldBetragZwei;
+    Geldbetrag _geldBetragEins;
+    Geldbetrag _geldBetragZwei;
+    Geldbetrag _geldBetragDrei;
 
-	public GeldbetragTest() {
+    public void testeAddiere()
+    {
 
-		_geldBetragEins = new Geldbetrag();
-		_geldBetragZwei = new Geldbetrag();
-	}
+    }
 
-	void testeAddiere() {
+    public void testeSubtrahieren()
+    {
 
-	}
+    }
 
-	@Test
-	public void testeKonvertiereString() {
+    public void testeIstSubtrahierenMoeglich()
+    {
 
-	}
+    }
 
-	@Test
-	public void testeHashCode() {
+    @Test
+    public void testeMultipliziere()
+    {
+        _geldBetragEins = Geldbetrag.get(20, 25);
+        _geldBetragZwei = Geldbetrag.get(20, 00);
 
-	}
+        assertTrue(Geldbetrag.get(40, 00), _geldBetragZwei.multipliziere(2));
+        assertFalse(Geldbetrag.get(81), _geldBetragEins.multipliziere(4));
 
-	@Test
-	public void testeEquals() {
+    }
 
-	}
+    @Test
+    public void testeIstMultiplizierenMeoglich()
+    {
+
+    }
+
+    @Test
+    public void testeKonvertiereString()
+    {
+
+    }
+
+    @Test
+    public void testeHashCode()
+    {
+
+    }
+
+    @Test
+    public void testeEquals()
+    {
+
+    }
 }
