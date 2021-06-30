fa0852934e0d2556f5b4afe075570dea3fa62551
Jan <jlbichel@gmail.com>
2021-06-30 10:20:46 +0200

    Dummy implementierungen in Geldbetrag und mehr Testfälle

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -3,73 +3,64 @@ package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Geldbetrag
-{
+public class Geldbetrag {
 
-    private int _euroanteil;
-    private int _centanteil;
-    private static Map<String, Geldbetrag> _werteMenge = new HashMap<String, Geldbetrag>();
+	private int _euroanteil;
+	private int _centanteil;
+	private static Map<String, Geldbetrag> _werteMenge = new HashMap<String, Geldbetrag>();
 
-    private Geldbetrag(int centbetrag)
-    {
-        _euroanteil = centbetrag / 100;
-        _centanteil = centbetrag % 100;
-    }
+	private Geldbetrag(int centbetrag) {
+		_euroanteil = centbetrag / 100;
+		_centanteil = centbetrag % 100;
+	}
 
-    private Geldbetrag(int euroanteil, int centanteil)
-    {
-        _euroanteil = euroanteil;
-        _centanteil = centanteil;
-    }
+	private Geldbetrag(int euroanteil, int centanteil) {
+		_euroanteil = euroanteil;
+		_centanteil = centanteil;
+	}
 
-    public static Geldbetrag select(int euroAnteil, int centAnteil)
-    {
-        String key = euroAnteil + "," + centAnteil;
-        if (!_werteMenge.containsKey(key))
-        {
-            _werteMenge.put(key, new Geldbetrag(euroAnteil, centAnteil));
-        }
-        return _werteMenge.get(key);
-    }
+	public static Geldbetrag select(int euroAnteil, int centAnteil) {
+		String key = euroAnteil + "," + centAnteil;
+		if (!_werteMenge.containsKey(key)) {
+			_werteMenge.put(key, new Geldbetrag(euroAnteil, centAnteil));
+		}
+		return _werteMenge.get(key);
+	}
 
-    public boolean istGueltigerEuroanteil(int euroanteil)
-    {
-        return false;
-    }
+	public boolean istGueltigerEuroanteil(int euroanteil) {
+		return false;
+	}
 
-    public boolean istGueltigerCentanteil(int centanteil)
-    {
-        return false;
-    }
+	public boolean istGueltigerCentanteil(int centanteil) {
+		return false;
+	}
 
-    public Geldbetrag addiere(Geldbetrag summand1, Geldbetrag summand2)
-    {
+	public Geldbetrag addiere(Geldbetrag summand1, Geldbetrag summand2) {
+		return Geldbetrag.select(0, 0);
+	}
 
-    }
+	public boolean istAddierenMoeglich(Geldbetrag betrag1, Geldbetrag betrag2) {
+		return true;
+	}
 
-    public boolean istAddierenMoeglich(Geldbetrag betrag1, Geldbetrag betrag2)
-    {
+	public Geldbetrag multipliziere(int faktor) {
+		return Geldbetrag.select(0, 0);
+	}
 
-    }
+	public boolean istMultiplizierenMoeglich(Geldbetrag betrag, int faktor) {
+		return true;
+	}
 
-    public Geldbetrag multipliziere(int faktor)
-    {
+	public int getEuroanteil() {
+		return _euroanteil;
+	}
 
-    }
+	public int getCentanteil() {
+		return _centanteil;
+	}
 
-    public boolean istMultiplizierenMoeglich(Geldbetrag betrag, int faktor)
-    {
-
-    }
-
-    public int getEuroanteil()
-    {
-        return _euroanteil;
-    }
-
-    public int getCentanteil()
-    {
-        return _centanteil;
-    }
+	public String konvertiereString() {
+		return "test";
+	}
 
 }
--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -1,17 +1,15 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class GeldbetragTest {
 
 	Geldbetrag _geldBetragEins;
 	Geldbetrag _geldBetragZwei;
-
-	public GeldbetragTest() {
-
-		_geldBetragEins = new Geldbetrag();
-		_geldBetragZwei = new Geldbetrag();
-	}
+	Geldbetrag _geldBetragDrei;
 
 	void testeAddiere() {
 
@@ -19,16 +17,30 @@ public class GeldbetragTest {
 
 	@Test
 	public void testeKonvertiereString() {
+		_geldBetragEins = Geldbetrag.select(0, 0);
+		_geldBetragZwei = Geldbetrag.select(1, 50);
 
+		assertEquals(_geldBetragEins.konvertiereString(), "0,00€");
+		assertEquals(_geldBetragEins.konvertiereString(), "1,50€");
 	}
 
 	@Test
-	public void testeHashCode() {
+	public void testeSelectGeldbetrag() {
+		_geldBetragEins = Geldbetrag.select(0, 0);
+		_geldBetragZwei = Geldbetrag.select(0, 0);
+		_geldBetragDrei = Geldbetrag.select(1, 0);
 
+		assertTrue(_geldBetragEins == _geldBetragZwei);
+		assertTrue(_geldBetragEins != _geldBetragDrei);
 	}
 
 	@Test
 	public void testeEquals() {
+		_geldBetragEins = Geldbetrag.select(0, 0);
+		_geldBetragZwei = Geldbetrag.select(0, 0);
+		_geldBetragDrei = Geldbetrag.select(1, 0);
 
+		assertTrue(_geldBetragEins.equals(_geldBetragZwei));
+		assertTrue(!_geldBetragEins.equals(_geldBetragDrei));
 	}
 }
