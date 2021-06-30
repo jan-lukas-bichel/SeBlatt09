5f8393c124b3c72592b4a7024f5156a68712ebc6
se2ng <84448498+se2ng@users.noreply.github.com>
2021-06-30 09:52:00 +0200

    Update Geldbetrag.java

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -1,9 +1,35 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
-public class Geldbetrag {
+import java.util.HashMap;
+import java.util.Map;
 
-	public Geldbetrag() {
-		// TODO Auto-generated constructor stub
-	}
+public class Geldbetrag
+{
+
+    private int _euroanteil;
+    private int _centanteil;
+    private static Map<String, Geldbetrag> _werteMenge = new HashMap<String, Geldbetrag>();
+
+    public Geldbetrag(int centbetrag)
+    {
+        _euroanteil = centbetrag / 100;
+        _centanteil = centbetrag % 100;
+    }
+
+    public Geldbetrag(int euroanteil, int centanteil)
+    {
+        _euroanteil = euroanteil;
+        _centanteil = centanteil;
+    }
+
+    public static Geldbetrag select(int euroAnteil, int centAnteil)
+    {
+        String key = euroAnteil + "," + centAnteil;
+        if (!_werteMenge.containsKey(key))
+        {
+            _werteMenge.put(key, new Geldbetrag(euroAnteil, centAnteil));
+        }
+        return _werteMenge.get(key);
+    }
 
 }
