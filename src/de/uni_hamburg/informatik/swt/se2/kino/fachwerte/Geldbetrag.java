f8e558c7f7d482c5490a3ff4a4e07ba41a05d2e3
se2ng <84448498+se2ng@users.noreply.github.com>
2021-06-30 09:58:57 +0200

    Update Geldbetrag.java

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -10,13 +10,13 @@ public class Geldbetrag
     private int _centanteil;
     private static Map<String, Geldbetrag> _werteMenge = new HashMap<String, Geldbetrag>();
 
-    public Geldbetrag(int centbetrag)
+    private Geldbetrag(int centbetrag)
     {
         _euroanteil = centbetrag / 100;
         _centanteil = centbetrag % 100;
     }
 
-    public Geldbetrag(int euroanteil, int centanteil)
+    private Geldbetrag(int euroanteil, int centanteil)
     {
         _euroanteil = euroanteil;
         _centanteil = centanteil;
@@ -30,6 +30,36 @@ public class Geldbetrag
             _werteMenge.put(key, new Geldbetrag(euroAnteil, centAnteil));
         }
         return _werteMenge.get(key);
+    }
+
+    public boolean istGueltigerEuroanteil(int euroanteil)
+    {
+        return false;
+    }
+
+    public boolean istGueltigerCentanteil(int centanteil)
+    {
+        return false;
+    }
+
+    public Geldbetrag addiere(Geldbetrag summand1, Geldbetrag summand2)
+    {
+
+    }
+
+    public boolean istAddierenMoeglich(Geldbetrag betrag1, Geldbetrag betrag2)
+    {
+
+    }
+
+    public Geldbetrag multipliziere(int faktor)
+    {
+
+    }
+
+    public boolean istMultiplizierenMoeglich(Geldbetrag betrag, int faktor)
+    {
+
     }
 
 }
